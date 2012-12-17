052c062fc503a6b79b75326768ef4884df586382
black-knight <dani_ande@yahoo.dk>
2012-12-17 21:50:26 +0100

    Deploy from github

--- scripts/deploy_from_github.sh
+++ scripts/deploy_from_github.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd ~/download
+rm -rf master EtikTak-master
+
 wget https://nodeload.github.com/black-knight/EtikTak/zip/master
 unzip master
 
@@ -8,11 +10,10 @@ cd EtikTak-master
 
 sed -i 's/\.sqlite3/.mysql/g' EtikTakProject/settings.py
 sed -i 's/database\.db/dnla_etiktak/g' EtikTakProject/settings.py
-sed -i 's/''USER'': ''''/''USER'': ''dnla_etiktak''/g' EtikTakProject/settings.py
-sed -i 's/''PASSWORD'': ''''/''PASSWORD'': ''REPLACE_ON_HOST''/g', EtikTakProject/settings.py
+sed -i "s/'USER': ''/'USER': 'dnla_etiktak'/g" EtikTakProject/settings.py
+sed -i "s/'PASSWORD': ''/'PASSWORD': 'REPLACE_ON_HOST'/g" EtikTakProject/settings.py
 
-mv * ~/webapps/etiktak/
+cp -rf * ~/webapps/etiktak/
 
 cd ~/download
-rm master EtikTak-master
-
+rm -rf master EtikTak-master
