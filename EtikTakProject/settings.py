6aff74f4746a8ae85b60db24c821db2e288ce4b4
black-knight <dani_ande@yahoo.dk>
2012-12-18 19:35:04 +0100

    Static files settings

--- EtikTakProject/settings.py
+++ EtikTakProject/settings.py
@@ -56,18 +56,18 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dnla/webapps/etiktak/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = ''
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/dnla/webapps/etiktak/static'
+    '/Users/daniel/projects/EtikTak/static'
 )
 
 # List of finder classes that know how to find static files in
