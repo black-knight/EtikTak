bfe812113973029247894807daac8652f54e6f48
black-knight <dani_ande@yahoo.dk>
2013-01-05 22:01:01 +0100

    Unit tests script

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/bash
 
-prog=$'\
-from EtikTakProject import settings\n\
-from subprocess import call\n\
-progs = [s[s.rfind(".")+1:len(s)] for s in settings.MY_APPS]\n\
-print progs\n\
-call(["./manage.py", "test"] + progs)\n\
-'
-
-python -c "$prog"
+./manage.py test
 
