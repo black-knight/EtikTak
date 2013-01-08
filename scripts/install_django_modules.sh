00162e391a7f9cf9d7c42ccf61bc4509a21451dd
black-knight <dani_ande@yahoo.dk>
2013-01-08 20:36:19 +0100

    Keyring support

--- scripts/install_django_modules.sh
+++ scripts/install_django_modules.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/bash
 
+# Webservice
 easy_install django-piston
-easy_install django_google_maps
 easy_install httplib2
-easy_install lettuce
 easy_install simplejson
+
+# Admin
+easy_install django_google_maps
+
+# Testing
+easy_install lettuce
 easy_install django_nose
 easy_install coverage
+
+# Keyring
+easy_install pycrypto
+easy_install keyring
